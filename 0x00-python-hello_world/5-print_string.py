bb6113aa55ca6eb2cde9b66ae275c1462ce5cb99
IreneMercy <irenemercy700@gmail.com>
2023-08-07 12:36:36 -0700

    print string

--- 0x00-python-hello_world/5-print_string.py
+++ 0x00-python-hello_world/5-print_string.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 
 
-def print_string():
-    str = "Holberton School"
-    print(3 * str)
-    print(str[0:9])
-
-
-print_string()
+str = "Holberton School"
+print(3 * str)
+print(str[0:9])
