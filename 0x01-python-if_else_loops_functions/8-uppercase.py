e9aedf298abd4f0f75b5c38500149ec242ec6ed1
IreneMercy <irenemercy700@gmail.com>
2023-08-08 08:43:34 -0700

    linked list

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 def uppercase(s):
     for char in s:
-        if 'a' <= char <= 'z':
+        if 97 <= char <= 122:
             print(chr(ord(char) - 32), end="")
         else:
             print(char, end="")
