d4cce08b978d63308fc2a7e0646c6de2c440fc4b
IreneMercy <irenemercy700@gmail.com>
2023-08-16 08:55:31 -0700

    multiply values

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -5,4 +5,4 @@ def multiply_by_2(a_dictionary):
     for key, value in a_dictionary.items():
         new_value = value * 2
         new_dictionary[key] = new_value
-    return new_dictionary
+    return (new_dictionary)
